cd794bede6e1df392dedc986d904ad9911117642
ToniYun <antonioyun2000@gmail.com>
2020-05-16 15:27:28 -0700

    Create UIScript.cs

---
+++ CSS385FInalProject/Assets/Character Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void GotoSettings()
+    {
+        SceneManager.LoadScene("Scenes/Settings");
+    }
+
+    public void GotoHighscores()
+    {
+        SceneManager.LoadScene("Scenes/Highscores");
+    }
+
+    public void GotoGame()
+    {
+        SceneManager.LoadScene("Scenes/Prototype test");
+    }
+
+    public void GotoIntro()
+    {
+        SceneManager.LoadScene("Scenes/Intro UI");
+    }
+}
