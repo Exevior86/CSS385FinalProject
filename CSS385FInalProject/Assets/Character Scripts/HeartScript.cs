c44d2a7fb5b41b075bad67164e8c601a0ded8100
Exevior86 <bucco1221@gmail.com>
2020-05-19 20:58:32 -0700

    New power ups
    Rapid fire power up added
    Health pick ups added
    new character ability: Sprint using shift key

---
+++ CSS385FInalProject/Assets/Character Scripts/HeartScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            CharacterStats.health += 5;
+            Destroy(this.gameObject);
+        }
+    }
+}
