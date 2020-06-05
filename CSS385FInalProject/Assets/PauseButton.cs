b357d8d1ef6f53eefaa0310e8af782dba122136c
ToniYun <antonioyun2000@gmail.com>
2020-06-05 01:03:35 -0700

    Pause

---
+++ CSS385FInalProject/Assets/PauseButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+    public void Pause()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
